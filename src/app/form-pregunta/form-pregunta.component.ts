87e9b92ea26a330b8b09d3c4df3eca3fb575b389
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-23 14:45:48 +0100

    agregar pregunta creado e implementado

--- src/app/form-pregunta/form-pregunta.component.ts
+++ src/app/form-pregunta/form-pregunta.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Pregunta } from '../models/pregunta.model';
+import { PreguntasService } from './preguntas.service';
 
 @Component({
   selector: 'app-form-pregunta',
@@ -12,9 +13,11 @@ export class FormPreguntaComponent implements OnInit {
   public titulo: string;
   public publicacion: string;
   public modoEdit = false;
-  constructor() { }
+  public preguntaIndex: number;
+  constructor(private preguntaService: PreguntasService) { }
 
   ngOnInit(): void {
+    
     this.initForm();
   }
 
@@ -26,7 +29,7 @@ export class FormPreguntaComponent implements OnInit {
       titulo = pregunta.titulo;
       publicacion = pregunta.publicacion;
     }
-    
+
     this.form = new FormGroup({
       'titulo' : new FormControl(titulo, Validators.required),
       'publicacion' : new FormControl(publicacion, Validators.required),
@@ -34,6 +37,10 @@ export class FormPreguntaComponent implements OnInit {
   }
 
   public publicar() {
-    
+    if (this.modoEdit) {
+      this.preguntaService.modificarPreguntas(this.form.value,this.preguntaIndex);
+    } else {
+      this.preguntaService.agregarPregunta(this.form.value);
+    }
   }
 }
--- src/app/form-pregunta/preguntas.service.ts
+++ src/app/form-pregunta/preguntas.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Pregunta } from '../models/pregunta.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreguntasService {
-  
+  private dataPreguntas = new Array<Pregunta>();
+  private preguntaCambios = new Subject<Array<Pregunta>>();
   constructor() { }
 
-  public agregarPregunta() {
+  public agregarPregunta(pregunta: Pregunta) {
+    this.dataPreguntas.push(pregunta);
+    this.preguntaCambios.next(this.dataPreguntas.slice());
+  }
 
+  public modificarPreguntas(preguntaAModificar: Pregunta, index: number) {
+    this.dataPreguntas[index] = preguntaAModificar;
+    this.preguntaCambios.next(this.dataPreguntas.slice());
   }
 }
